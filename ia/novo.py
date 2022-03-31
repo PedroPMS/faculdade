e3ecefabe1749786ab7f091a0e87535e307a6ebb
Pedro Silva <p.paulo0512@gmail.com>
2022-03-31 18:48:48 -0300

    desenhando caminho

--- ia/novo.py
+++ ia/novo.py
@@ -1,12 +1,15 @@
 import heapq
 import pandas
 import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import colors
 from scipy.spatial.distance import cityblock
 
 def heuristica(vector_a, vector_b):
+    # retorna o caminho de Manhattan
     return cityblock([vector_a[0], vector_a[1]], [vector_b[0], vector_b[1]])
 
-def astar(matrix, inicio, objetivo):
+def aestrela(matrix, inicio, objetivo):
     # vizinho do local atual: cima, baixo, esqueda, direita
     vizinhos = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
@@ -19,7 +22,7 @@ def astar(matrix, inicio, objetivo):
     # adiciona na lista aberta a posição inicial
     heapq.heappush(listaAberta, (g[inicio], inicio))
 
-    # enquanto tiver elemntos na lista aberta
+    # enquanto tiver elementos na lista aberta
     while listaAberta:
         # recupera a posição atual na matix
         atual = heapq.heappop(listaAberta)[1]
@@ -28,6 +31,7 @@ def astar(matrix, inicio, objetivo):
         if atual == objetivo:
             data = []
             while atual in posicaoAnterior:
+                data.append(atual)
                 atual = posicaoAnterior[atual]
             return data
 
@@ -41,10 +45,10 @@ def astar(matrix, inicio, objetivo):
                     if matrix[vizinho[0]][vizinho[1]] == 1:
                         continue
                 else:
-                    # encontrou em uma parede
+                    # encontrou uma parede
                     continue
             else:
-                # encontrou em uma parede
+                # encontrou uma parede
                 continue
 
             # se já foi visitado, pula
@@ -60,70 +64,59 @@ def astar(matrix, inicio, objetivo):
 
     return False
 
+def caminhoMatrix(matrix, rota, objetivo):
+    caminhoDesenhado = np.copy(matrix.astype(str))
+    caminhoDesenhado[objetivo[0]][objetivo[1]] = '*'
+
+    for i in range(len(rota)):
+        linhaAtual = rota[i][0]
+        colunaAtual = rota[i][1]
+
+        if linhaAtual != objetivo[0] or colunaAtual != objetivo[1]:
+            linhaPosterior = rota[i+1][0]
+            colunaPosterior = rota[i+1][1]
+
+        if colunaAtual > colunaPosterior:
+            caminhoDesenhado[linhaAtual][colunaAtual] = '<'
+        if colunaAtual < colunaPosterior:
+            caminhoDesenhado[linhaAtual][colunaAtual] = '>'
+        if linhaAtual > linhaPosterior:
+            caminhoDesenhado[linhaAtual][colunaAtual] = '^'
+        if linhaPosterior > linhaAtual:
+            caminhoDesenhado[linhaAtual][colunaAtual] = 'v'
+    print (pandas.DataFrame(caminhoDesenhado))
+
+def caminhoImagem(matrix, rota, inicio, objetivo):
+    caminhoDesenhado = np.copy(matrix.astype(int))
+
+    for i in range(len(rota)):
+        linhaAtual = rota[i][0]
+        colunaAtual = rota[i][1]
+
+        caminhoDesenhado[linhaAtual][colunaAtual] = 3
+
+    caminhoDesenhado[inicio[0]][inicio[1]] = 2
+    caminhoDesenhado[objetivo[0]][objetivo[1]] = 9
+    # print (pandas.DataFrame(caminhoDesenhado))
+
+    cmap = colors.ListedColormap(['white', 'black', 'green', 'blue', 'red'])
+    bounds=[0,1,2,3,8,9]
+    norm = colors.BoundaryNorm(bounds, cmap.N)
+
+    plt.imshow(caminhoDesenhado, interpolation='nearest', cmap=cmap, norm=norm)
+    plt.savefig('caminho.png')
+
 
 matrix = np.loadtxt('matrix.txt').astype(int)
-inicio = (0, 0)
-objetivo = (14, 14)
+inicio = (7, 12)
+objetivo = (0, 3)
 
-route = astar(matrix, inicio, objetivo)
-# route = route + [inicio]
-# route = route[::-1]
+rota = aestrela(matrix, inicio, objetivo)
+rota = rota + [inicio]
+rota = rota[::-1]
 
-caminhoDesenhado = np.copy(matrix.astype(str))
-caminhoDesenhado[objetivo[0]][objetivo[1]] = '*'
-
-for i in range(len(route)):
-    linhaAtual = route[i][0]
-    colunaAtual = route[i][1]
-
-    if linhaAtual != objetivo[0] or colunaAtual != objetivo[1]:
-        linhaPosterior = route[i+1][0]
-        colunaPosterior = route[i+1][1]
-
-    if colunaAtual > colunaPosterior:
-        caminhoDesenhado[linhaAtual][colunaAtual] = '<'
-    if colunaAtual < colunaPosterior:
-        caminhoDesenhado[linhaAtual][colunaAtual] = '>'
-        # print (pandas.DataFrame(test))
-    if linhaAtual > linhaPosterior:
-        caminhoDesenhado[linhaAtual][colunaAtual] = '^'
-    if linhaPosterior > linhaAtual:
-        caminhoDesenhado[linhaAtual][colunaAtual] = 'v'
-print (pandas.DataFrame(caminhoDesenhado))
+caminhoMatrix(matrix, rota, objetivo)
+caminhoImagem(matrix, rota, inicio, objetivo)
 
 
-# extract x and y coordinates from route list
-# x_coords = []
-# y_coords = []
 
-
-# for i, item in enumerate(route):
-#     with plt.xkcd():
-#         x = route[i][0]
-#         y = route[i][1]
-#         x_coords.append(x)
-#         y_coords.append(y)
-#         fig, ax = plt.subplots(figsize=(8, 8))
-#         ax.imshow(matrix, cmap=plt.cm.Dark2)
-#         ax.scatter(inicio[1], inicio[0], marker="*", color="yellow", s=200)
-#         ax.scatter(objetivo[1], objetivo[0], marker="+", color="red", s=200)
-#         ax.plot(y_coords, x_coords, color="black")
-#         plt.xticks([])
-#         plt.yticks([])
-#         plt.title("A* algorithm step " + str(i))
-
-#         # Output numbering in a ffmpeg compatible way!
-#         if i < 10:
-#             filename = "mazestep_00%d.png" % i
-#         elif (i > 9) & (i < 100):
-#             filename = "mazestep_0%d.png" % i
-#         else:
-#             filename = "mazestep_%d.png" % i
-
-#         plt.savefig(filename)
-#         plt.close()
-# plt.show()
-
-
-# need to then run from the shell:
-# ffmpeg -v warning -i mazestep_%03d.png -y out.gif
